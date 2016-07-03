46364e91342b59b7975454f65d3709d779559380
Aicha <aichayou2010@gmail.com>
2016-07-03 18:21:28 +0200

    gestion du multiplicatateur

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-var bouton = document.getElementById("bouton");
-var click = 0;
-function test() {
-    click += 1;
-   document.getElementById("click").innerHTML = click;
+
+
+		var bouton = document.getElementById("bouton");
+		var multiplication = document.getElementById("compteur");
+		var Score = document.getElementById("score");
+		score = 0;
+		compteur = 1;
+		tarif= 10;
+function affichage(){
+	Score.innerHTML = score ; 
+
 }
+
+
+function colMult(){
+   multiplication.innerHTML = " multiplicateur x " + compteur + " prix : " + valeurMult() ;
+}
+
+
+function clic() {
+	score = score + compteur ;
+	affichage();
+}
+
+
+function valeurMult(){
+	return 10 * compteur ;
+}
+
+
+function achat(){
+	if ( score >= valeurMult() ) {
+		score = score - tarif ;
+		compteur = compteur + 1 ;
+		affichage();
+		colMult();
+	} 
+		     }
+
+affichage();
+colMult();
+
