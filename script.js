24dc44112f3bd271acaa9ae8460dee934bf8483b
Aicha <aichayou2010@gmail.com>
2016-07-04 21:39:40 +0200

    utilisation de la fonction setinterval pour gerer l' autoclic

--- script.js
+++ script.js
@@ -3,12 +3,15 @@
 		var bouton = document.getElementById("bouton");
 		var multiplication = document.getElementById("compteur");
 		var Score = document.getElementById("score");
+		var test=document.getElementById("auto");
 		score = 0;
 		compteur = 1;
 		tarif= 10;
+		nombreAuto = 1;
+	
+   
 function affichage(){
-	Score.innerHTML = score ; 
-
+	Score.innerHTML = " score:  " + score ; 
 }
 
 
@@ -20,11 +23,10 @@ function colMult(){
 function clic() {
 	score = score + compteur ;
 	affichage();
+
 }
-
-
 function valeurMult(){
-	return 10 * compteur ;
+	return 50 * compteur ;
 }
 
 
@@ -35,8 +37,35 @@ function achat(){
 		affichage();
 		colMult();
 	} 
+	else {
+        alert("Votre score est insuffisant !");
+    }
 		     }
+
+function autoclic(){
+	
+	var clicAuto = setInterval(clic, 1000 );
+		     }
+
+function achatAutoclic(){
+    if ( score >= prixAchat()) {
+		score = score - prixAchat() ;
+        autoclic();
+	} else {
+		 alert("clic un peu plus");
+	}
+							}
+function afficheAuto(){
+    test.innerHTML = " je fais des clics automatiques x " + nombreAuto + " prix:  " + prixAchat() ;
+}
+
+
+function prixAchat(){
+	return 200 * nombreAuto;
+}
 
 affichage();
 colMult();
+afficheAuto();
+
 
